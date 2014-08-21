aedf691becb30a0614e09367685a126cffe75c44
Kent Lovelace <klovelacek@gmail.com>
2014-08-20 20:30:44 -0400

    fixed carousel images and js functionality

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -38,49 +38,122 @@ $(function($) {
 					.setTween(tweenFour)
 					.addTo(controller);
 
-		//SCENE FIVE MARTIN AGENCY ---- building tween in from left, logo from bottom
+		//SCENE FIVE MARTIN AGENCY building ----- tween in from left, logo from bottom
 
-			var tweenFive = TweenMax.to("#animate5a", 0.75, {left: -80});
+			var tweenFive = TweenMax.to("#animate5a", 0.75, {left: 2});
 			// build scene
 			var sceneFive = new ScrollScene({triggerElement: "#trigger5"})
 					.setTween(tweenFive)
 					.addTo(controller);
 
+					console.log("hello");
 
-			// PORTFOLIO CAROUSEL
+	// PORTFOLIO CAROUSEL SECTION 8
 
-				$(".nextLink").on("click", function(e){
+		$(".nextLink").on("click", function(e){
 
-					var currentActiveImage = $(".image-shown");
+			var currentActiveImage = $(".image-shown");
+			var nextActiveImage = currentActiveImage.next();
+
+			if(nextActiveImage.length == 0)
+				{
+					nextActiveImage = $(".carousel-inner img").first();
+				}
+
+				currentActiveImage.removeClass("image-shown").addClass("image-hidden").css("z-index", -10);
+				nextActiveImage.addClass("image-shown").removeClass("image-hidden").css("z-index", 20);
+				$(".carousel-inner img").not([currentActiveImage, nextActiveImage]).css("z-index", 1);
+
+				e.preventDefault();
+		});
+
+		$(".previousLink").on("click", function(e){
+
+			var currentActiveImage = $(".image-shown");
+			var nextActiveImage = currentActiveImage.prev();
+
+			if(nextActiveImage.length == 0)
+				{
+					nextActiveImage = $(".carousel-inner img").last();
+				}
+
+				currentActiveImage.removeClass("image-shown").addClass("image-hidden").css("z-index", -10);
+				nextActiveImage.addClass("image-shown").removeClass("image-hidden").css("z-index", 1);
+				$(".carousel-inner img").not([currentActiveImage, nextActiveImage]).css("z-index", 1);
+
+				e.preventDefault();
+		})
+
+		// PORTFOLIO CAROUSEL SECTION 10
+
+			$(".nextLink2").on("click", function(e){
+
+				var currentActiveImage = $(".image-shown2");
+				var nextActiveImage = currentActiveImage.next();
+
+				if(nextActiveImage.length == 0)
+					{
+						nextActiveImage = $(".carousel-inner2 img").first();
+					}
+
+					currentActiveImage.removeClass("image-shown2").addClass("image-hidden2").css("z-index", -10);
+					nextActiveImage.addClass("image-shown2").removeClass("image-hidden2").css("z-index", 20);
+					$(".carousel-inner2 img").not([currentActiveImage, nextActiveImage]).css("z-index", 1);
+
+					e.preventDefault();
+			});
+
+			$(".previousLink2").on("click", function(e){
+
+				var currentActiveImage = $(".image-shown2");
+				var nextActiveImage = currentActiveImage.prev();
+
+				if(nextActiveImage.length == 0)
+					{
+						nextActiveImage = $(".carousel-inner2 img").last();
+					}
+
+					currentActiveImage.removeClass("image-shown2").addClass("image-hidden2").css("z-index", -10);
+					nextActiveImage.addClass("image-shown2").removeClass("image-hidden2").css("z-index", 1);
+					$(".carousel-inner2 img").not([currentActiveImage, nextActiveImage]).css("z-index", 1);
+
+					e.preventDefault();
+			})
+
+			// PORTFOLIO CAROUSEL SECTION 12
+
+				$(".nextLink3").on("click", function(e){
+
+					var currentActiveImage = $(".image-shown3");
 					var nextActiveImage = currentActiveImage.next();
 
 					if(nextActiveImage.length == 0)
 						{
-							nextActiveImage = $(".carousel-inner img").first();
+							nextActiveImage = $(".carousel-inner3 img").first();
 						}
 
-						currentActiveImage.removeClass("image-shown").addClass("image-hidden").css("z-index", -10);
-						nextActiveImage.addClass("image-shown").removeClass("image-hidden").css("z-index", 20);
-						$(".carousel-inner img").not([currentActiveImage, nextActiveImage]).css("z-index", 1);
+						currentActiveImage.removeClass("image-shown3").addClass("image-hidden3").css("z-index", -10);
+						nextActiveImage.addClass("image-shown3").removeClass("image-hidden3").css("z-index", 20);
+						$(".carousel-inner3 img").not([currentActiveImage, nextActiveImage]).css("z-index", 1);
 
 						e.preventDefault();
 				});
 
-				$(".previousLink").on("click", function(e){
+				$(".previousLink3").on("click", function(e){
 
-					var currentActiveImage = $(".image-shown");
+					var currentActiveImage = $(".image-shown3");
 					var nextActiveImage = currentActiveImage.prev();
 
 					if(nextActiveImage.length == 0)
 						{
-							nextActiveImage = $(".carousel-inner img").last();
+							nextActiveImage = $(".carousel-inner3 img").last();
 						}
 
-						currentActiveImage.removeClass("image-shown").addClass("image-hidden").css("z-index", -10);
-						nextActiveImage.addClass("image-shown").removeClass("image-hidden").css("z-index", 1);
+						currentActiveImage.removeClass("image-shown3").addClass("image-hidden3").css("z-index", -10);
+						nextActiveImage.addClass("image-shown3").removeClass("image-hidden3").css("z-index", 1);
+						$(".carousel-inner3 img").not([currentActiveImage, nextActiveImage]).css("z-index", 1);
 
 						e.preventDefault();
 				})
-			// }
 
-	});
+});
