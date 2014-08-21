dc7521aa234b30a2ed0d32d1c96dbd564b1519a0
Kent Lovelace <klovelacek@gmail.com>
2014-08-21 10:12:01 -0400

    fixed/added animation. Redesigned sect. 5

--- scripts/main.js
+++ scripts/main.js
@@ -21,7 +21,7 @@ $(function($) {
 
     // SCENE THREE VCU ---- fly in and scale from right
 
-		var tweenThree = {duration: 200, offset: -10};
+		var tweenThree = {duration: 1, offset: -10};
 		var elements = $(".vculayer1");
     // build scene
 		new ScrollScene()
@@ -30,130 +30,168 @@ $(function($) {
   			.triggerElement(".vculayer1")
   			.setTween(TweenMax.from(".vculayer1", 1, {scale: 750, ease: Back.easeOut}));
 
-    	//SCENE FOUR MEET JAIVER ---- tween in from right
+  	//SCENE FOUR MEET JAIVER ---- tween in from right
 
-			var tweenFour = TweenMax.to("#animate4", 1.5, {left: 5});
-	// 			// build scene
-			var sceneFour = new ScrollScene({triggerElement: "#trigger4"})
-					.setTween(tweenFour)
-					.addTo(controller);
+		var tweenFour = TweenMax.to("#animate4", 1.5, {left: 5});
+// 			// build scene
+		var sceneFour = new ScrollScene({triggerElement: "#trigger4"})
+				.setTween(tweenFour)
+				.addTo(controller);
 
-		//SCENE FIVE MARTIN AGENCY building ----- tween in from left, logo from bottom
 
-			var tweenFive = TweenMax.to("#animate5a", 0.75, {left: 2});
-			// build scene
-			var sceneFive = new ScrollScene({triggerElement: "#trigger5"})
-					.setTween(tweenFive)
-					.addTo(controller);
+		//SCENE FOUR MEET SHARON ---- tween in from bottom
 
-					console.log("hello");
+		var tweenNine = TweenMax.to("#animate9", 1.5, {top: 5});
+// 			// build scene
+		var sceneNine = new ScrollScene({triggerElement: "#trigger9"})
+				.setTween(tweenNine)
+				.addTo(controller);
 
-	// PORTFOLIO CAROUSEL SECTION 8
 
-		$(".nextLink").on("click", function(e){
+// MARTIN AGENCY CAROUSEL SECTION 5
 
-			var currentActiveImage = $(".image-shown");
+$(".nextLink0").on("click", function(e){
+
+		var currentActiveImage = $(".image-shown0");
+		var nextActiveImage = currentActiveImage.next();
+
+		if(nextActiveImage.length == 0)
+			{
+				nextActiveImage = $(".carousel-inner0 img").first();
+			}
+
+			currentActiveImage.removeClass("image-shown0").addClass("image-hidden0").css("z-index", -10);
+			nextActiveImage.addClass("image-shown0").removeClass("image-hidden0").css("z-index", 20);
+			$(".carousel-inner0 img").not([currentActiveImage, nextActiveImage]).css("z-index", 1);
+
+			e.preventDefault();
+	});
+
+$(".previousLink0").on("click", function(e){
+
+		var currentActiveImage = $(".image-shown0");
+		var nextActiveImage = currentActiveImage.prev();
+
+		if(nextActiveImage.length == 0)
+			{
+				nextActiveImage = $(".carousel-inner0 img").last();
+			}
+
+			currentActiveImage.removeClass("image-shown0").addClass("image-hidden0").css("z-index", -10);
+			nextActiveImage.addClass("image-shown0").removeClass("image-hidden0").css("z-index", 1);
+			$(".carousel-inner0 img").not([currentActiveImage, nextActiveImage]).css("z-index", 1);
+
+			e.preventDefault();
+})
+
+
+
+// PORTFOLIO CAROUSEL SECTION 8
+
+$(".nextLink").on("click", function(e){
+
+	var currentActiveImage = $(".image-shown");
+	var nextActiveImage = currentActiveImage.next();
+
+	if(nextActiveImage.length == 0)
+		{
+			nextActiveImage = $(".carousel-inner img").first();
+		}
+
+		currentActiveImage.removeClass("image-shown").addClass("image-hidden").css("z-index", -10);
+		nextActiveImage.addClass("image-shown").removeClass("image-hidden").css("z-index", 20);
+		$(".carousel-inner img").not([currentActiveImage, nextActiveImage]).css("z-index", 1);
+
+		e.preventDefault();
+});
+
+$(".previousLink").on("click", function(e){
+
+	var currentActiveImage = $(".image-shown");
+	var nextActiveImage = currentActiveImage.prev();
+
+	if(nextActiveImage.length == 0)
+		{
+			nextActiveImage = $(".carousel-inner img").last();
+		}
+
+		currentActiveImage.removeClass("image-shown").addClass("image-hidden").css("z-index", -10);
+		nextActiveImage.addClass("image-shown").removeClass("image-hidden").css("z-index", 1);
+		$(".carousel-inner img").not([currentActiveImage, nextActiveImage]).css("z-index", 1);
+
+		e.preventDefault();
+})
+
+// PORTFOLIO CAROUSEL SECTION 10
+
+	$(".nextLink2").on("click", function(e){
+
+		var currentActiveImage = $(".image-shown2");
+		var nextActiveImage = currentActiveImage.next();
+
+		if(nextActiveImage.length == 0)
+			{
+				nextActiveImage = $(".carousel-inner2 img").first();
+			}
+
+			currentActiveImage.removeClass("image-shown2").addClass("image-hidden2").css("z-index", -10);
+			nextActiveImage.addClass("image-shown2").removeClass("image-hidden2").css("z-index", 20);
+			$(".carousel-inner2 img").not([currentActiveImage, nextActiveImage]).css("z-index", 1);
+
+			e.preventDefault();
+	});
+
+	$(".previousLink2").on("click", function(e){
+
+		var currentActiveImage = $(".image-shown2");
+		var nextActiveImage = currentActiveImage.prev();
+
+		if(nextActiveImage.length == 0)
+			{
+				nextActiveImage = $(".carousel-inner2 img").last();
+			}
+
+			currentActiveImage.removeClass("image-shown2").addClass("image-hidden2").css("z-index", -10);
+			nextActiveImage.addClass("image-shown2").removeClass("image-hidden2").css("z-index", 1);
+			$(".carousel-inner2 img").not([currentActiveImage, nextActiveImage]).css("z-index", 1);
+
+			e.preventDefault();
+	})
+
+	// PORTFOLIO CAROUSEL SECTION 12
+
+		$(".nextLink3").on("click", function(e){
+
+			var currentActiveImage = $(".image-shown3");
 			var nextActiveImage = currentActiveImage.next();
 
 			if(nextActiveImage.length == 0)
 				{
-					nextActiveImage = $(".carousel-inner img").first();
+					nextActiveImage = $(".carousel-inner3 img").first();
 				}
 
-				currentActiveImage.removeClass("image-shown").addClass("image-hidden").css("z-index", -10);
-				nextActiveImage.addClass("image-shown").removeClass("image-hidden").css("z-index", 20);
-				$(".carousel-inner img").not([currentActiveImage, nextActiveImage]).css("z-index", 1);
+				currentActiveImage.removeClass("image-shown3").addClass("image-hidden3").css("z-index", -10);
+				nextActiveImage.addClass("image-shown3").removeClass("image-hidden3").css("z-index", 20);
+				$(".carousel-inner3 img").not([currentActiveImage, nextActiveImage]).css("z-index", 1);
 
 				e.preventDefault();
 		});
 
-		$(".previousLink").on("click", function(e){
+		$(".previousLink3").on("click", function(e){
 
-			var currentActiveImage = $(".image-shown");
+			var currentActiveImage = $(".image-shown3");
 			var nextActiveImage = currentActiveImage.prev();
 
 			if(nextActiveImage.length == 0)
 				{
-					nextActiveImage = $(".carousel-inner img").last();
+					nextActiveImage = $(".carousel-inner3 img").last();
 				}
 
-				currentActiveImage.removeClass("image-shown").addClass("image-hidden").css("z-index", -10);
-				nextActiveImage.addClass("image-shown").removeClass("image-hidden").css("z-index", 1);
-				$(".carousel-inner img").not([currentActiveImage, nextActiveImage]).css("z-index", 1);
+				currentActiveImage.removeClass("image-shown3").addClass("image-hidden3").css("z-index", -10);
+				nextActiveImage.addClass("image-shown3").removeClass("image-hidden3").css("z-index", 1);
+				$(".carousel-inner3 img").not([currentActiveImage, nextActiveImage]).css("z-index", 1);
 
 				e.preventDefault();
 		})
-
-		// PORTFOLIO CAROUSEL SECTION 10
-
-			$(".nextLink2").on("click", function(e){
-
-				var currentActiveImage = $(".image-shown2");
-				var nextActiveImage = currentActiveImage.next();
-
-				if(nextActiveImage.length == 0)
-					{
-						nextActiveImage = $(".carousel-inner2 img").first();
-					}
-
-					currentActiveImage.removeClass("image-shown2").addClass("image-hidden2").css("z-index", -10);
-					nextActiveImage.addClass("image-shown2").removeClass("image-hidden2").css("z-index", 20);
-					$(".carousel-inner2 img").not([currentActiveImage, nextActiveImage]).css("z-index", 1);
-
-					e.preventDefault();
-			});
-
-			$(".previousLink2").on("click", function(e){
-
-				var currentActiveImage = $(".image-shown2");
-				var nextActiveImage = currentActiveImage.prev();
-
-				if(nextActiveImage.length == 0)
-					{
-						nextActiveImage = $(".carousel-inner2 img").last();
-					}
-
-					currentActiveImage.removeClass("image-shown2").addClass("image-hidden2").css("z-index", -10);
-					nextActiveImage.addClass("image-shown2").removeClass("image-hidden2").css("z-index", 1);
-					$(".carousel-inner2 img").not([currentActiveImage, nextActiveImage]).css("z-index", 1);
-
-					e.preventDefault();
-			})
-
-			// PORTFOLIO CAROUSEL SECTION 12
-
-				$(".nextLink3").on("click", function(e){
-
-					var currentActiveImage = $(".image-shown3");
-					var nextActiveImage = currentActiveImage.next();
-
-					if(nextActiveImage.length == 0)
-						{
-							nextActiveImage = $(".carousel-inner3 img").first();
-						}
-
-						currentActiveImage.removeClass("image-shown3").addClass("image-hidden3").css("z-index", -10);
-						nextActiveImage.addClass("image-shown3").removeClass("image-hidden3").css("z-index", 20);
-						$(".carousel-inner3 img").not([currentActiveImage, nextActiveImage]).css("z-index", 1);
-
-						e.preventDefault();
-				});
-
-				$(".previousLink3").on("click", function(e){
-
-					var currentActiveImage = $(".image-shown3");
-					var nextActiveImage = currentActiveImage.prev();
-
-					if(nextActiveImage.length == 0)
-						{
-							nextActiveImage = $(".carousel-inner3 img").last();
-						}
-
-						currentActiveImage.removeClass("image-shown3").addClass("image-hidden3").css("z-index", -10);
-						nextActiveImage.addClass("image-shown3").removeClass("image-hidden3").css("z-index", 1);
-						$(".carousel-inner3 img").not([currentActiveImage, nextActiveImage]).css("z-index", 1);
-
-						e.preventDefault();
-				})
 
 });
